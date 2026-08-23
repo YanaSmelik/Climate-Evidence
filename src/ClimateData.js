de9cac8c8b6cd4f231941759f3576733883c826b
yana smelyk <smelikyana@gmail.com>
2026-08-23 12:13:53 +0200

    Add buttons and dummy data for Topics component

--- src/ClimateData.js
+++ src/ClimateData.js
@@ -1,27 +1,55 @@
 import { useState } from "react";
 import globalCO2Data from "./data/globalCO2Data";
+import globalOceanTemperatureRise from "./data/globalOceanTemperatureRise";
+import globalSeaLevelRise from "./data/globalSeaLevelRise";
+import globalTemperatureRise from "./data/globalTemperatureRise";
 import Topics from "./Topics";
 
 function ClimateData() {
   const [topic, setTopic] = useState("");
+  let listDataByYears = [];
+   const topicDescription = {
+    CO2: 'Average carbon dioxide (CO₂) levels in the atmosphere worldwide from xxxx to xxxx',
+    oceanTempRise: 'Yearly global average sea surface temperature rise from xxxx to xxxx (in degrees Celsius)',
+    seaLevelRise: 'Yearly change in global mean sea level, as measured by satellite altimetry, from xxxx to xxxx',
+    globalTempRise: 'Yearly surface temperature rise from xxxx to xxxx'
+  }
 
   if (topic === "CO2") {
-    const listDataByYears = globalCO2Data.map((yearData) => (
+    listDataByYears = globalCO2Data.map((yearData) => (
       <li key={yearData.year}>
         {yearData.year + ": " + yearData.cO2Tons + " t."}
       </li>
     ));
-    return (
-      <div>
-        <h1>Climate Change Data</h1>
-        <ul>{listDataByYears}</ul>
-      </div>
-    );
   }
+  if (topic === "oceanTempRise") {
+    listDataByYears = globalOceanTemperatureRise.map((yearData) => (
+      <li key={yearData.year}>
+        {yearData.year + ": " + yearData.temperatureRise + " cm"}
+      </li>
+    ));
+  }
+  if (topic === "seaLevelRise") {
+    listDataByYears = globalSeaLevelRise.map((yearData) => (
+      <li key={yearData.year}>
+        {yearData.year + ": " + yearData.seaLevelRise + " cm"}
+      </li>
+    ));
+  }
+  if (topic === "globalTempRise") {
+    listDataByYears = globalTemperatureRise.map((yearData) => (
+      <li key={yearData.year}>
+        {yearData.year + ": " + yearData.temperatureRise + " °C"}
+      </li>
+    ));
+  }
+
   return (
     <div>
       <h1>Climate Change Data</h1>
-      <Topics setTopic={setTopic}/>
+      <Topics setTopic={setTopic} />
+      <p>{topicDescription[topic]}</p>
+      <ul>{listDataByYears}</ul>
     </div>
   );
 }
--- src/Topics.js
+++ src/Topics.js
@@ -1,10 +1,41 @@
-
 function Topics(props) {
+  let topicButtons = [
+    {
+      nameAttribute: "CO2",
+      buttonName: "CO2 Data",
+    },
+    {
+      nameAttribute: "oceanTempRise",
+      buttonName: "Ocean temperature rise",
+    },
+    {
+      nameAttribute: "seaLevelRise",
+      buttonName: "Sea level rise",
+    },
+    {
+      nameAttribute: "globalTempRise",
+      buttonName: "Global temperature rise",
+    },
+  ];
 
+  const topicButtonsJSX = topicButtons.map((topic) => {
     return (
-        <button onClick={(event) => {props.setTopic(event.target.name)}} name='CO2'>CO2 Data</button>
+      <button key={topic.nameAttribute}
+        onClick={(event) => {
+          props.setTopic(event.target.name);
+        }}
+        name={topic.nameAttribute}
+      >
+        {topic.buttonName}
+      </button>
     );
+  });
 
+  return (
+    <div>
+     {topicButtonsJSX}
+    </div>
+  );
 }
 
 export default Topics;
