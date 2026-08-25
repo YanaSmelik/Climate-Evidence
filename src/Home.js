e5e930788a6ebcf3f926c8648a135c45cc1e6e04
yana smelyk <smelikyana@gmail.com>
2026-08-25 20:11:17 +0200

    Add description to Home component. Change its style

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,9 @@ import styles from './style/home.module.css';
 function Home() {
     return (
         <div className={styles.main}>
-        <h1 className={styles.mainText}>Welcom to the Climate Evidence!</h1>
+        <h1 className={styles.mainText}>Climate Change Evidence</h1>
+        <p className={styles.subText}>There is unequivocal evidence that Earth is warming at an unprecedented rate.</p>
+        <p className={styles.subText}>Human activity is the principal cause.</p>
         </div>
     );
 }
