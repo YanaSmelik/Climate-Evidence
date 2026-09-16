40227689670208771775602370af7183720686b6
Yana <smelikyana@gmail.com>
2026-09-16 16:04:54 +0200

    Add support for domain paramerter change in Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,14 +1,43 @@
-import { CartesianGrid, Line, Legend, LineChart, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Line, Legend, LineChart, XAxis, YAxis, Tooltip } from 'recharts';
 
-export default function Chart(props) {
-    console.log(props.data);
+const topicsDomainValues = [
+    {
+        "topic": "CO2",
+        "domainValues": [300, 450]
+    },
+     {
+        "topic": "oceanTempRise",
+        "domainValues": [-200, 300]
+    },
+     {
+        "topic": "seaLevelRise",
+        "domainValues": [-100, 100]
+    },
+     {
+        "topic": "globalTempRise",
+        "domainValues": [-0.6, 1.6]
+    },
+]
+
+ function Chart(props) {
+
+    //TODO: adjust chart y axis domain for each topic
+    //TODO: adjust x axis tick for each topic
     return (
-        <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }} responsive data={props.data}>
-            <CartesianGrid />
-            <Line dataKey="value" />
-            <XAxis dataKey="label"/>
-            <YAxis />
-            <Legend />
+        <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 600 }} responsive data={props.data} margin={{
+            top: 20,
+            right: 20,
+            bottom: 5,
+            left: 0,
+        }}>
+            <CartesianGrid strokeDasharray="5 5"/>
+            <Line dataKey="value" type="monotone" strokeWidth={2} name="My data series name"/>
+            <XAxis dataKey="label" />
+            <YAxis label={{ value: 'UV', position: 'insideLeft', angle: -90 }} domain={[-0.6, 1.6]}/>
+            <Legend position="insideBottomRight"  />
+            <Tooltip />
         </LineChart>
     );
 }
+
+ export default Chart;
